94f69fceae2a549d028c64152c1a1502f0aae6d8
Brett Huneycutt <brett@1000memories.com>
2013-09-02 16:55:29 -0700

    properly redirect from the sign up and login pages

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UsersController do
   describe "POST create" do
     context "with valid parameters" do
-      it "renders the new template with errors" do
+      it "redirects to create a new league" do
         post :create, 'user' => {
           'name' => "Peter GeGe",
           'email' => 'peter@geegee.com',
@@ -12,6 +12,19 @@ describe UsersController do
         }
 
         expect(response).to redirect_to(new_league_path)
+      end
+
+      context "and redirect_to specified" do
+        it "redirects to specified path" do
+          post :create, 'redirect_to' => '/', 'user' => {
+            'name' => "Peter GeGe",
+            'email' => 'peter@geegee.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+          }
+
+          expect(response).to redirect_to('/')
+        end
       end
     end
 
