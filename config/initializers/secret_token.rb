acf10033856b1554db19055d2bd8a47b395c4bed
Brett Huneycutt <brett@1000memories.com>
2013-09-02 13:48:48 -0700

    install rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FantasyRocket::Application.config.secret_key_base = '930237580d27b666e069df16eefe4ef537015b8685fc98acb055b83db4ac5c05bfdf48984d7697c34f7b8e550995e96d0a1572e23ba67181b0663755916dc467'
