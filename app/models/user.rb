6ecc012b6c5eca61a60d634515b08d995c5eec53
Peter Graham <peterghm@gmail.com>
2013-09-02 15:17:11 -0700

    League model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, email: true
+
+  has_many :leagues, foreign_key: "commissioner_id", inverse_of: :commissioner
 end
