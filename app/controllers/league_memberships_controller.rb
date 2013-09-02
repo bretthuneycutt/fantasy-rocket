e0e0551b42d24dc21e61cb74a0f4899147c59ea4
Brett Huneycutt <brett@1000memories.com>
2013-09-02 16:53:17 -0700

    button to join league

--- app/controllers/league_memberships_controller.rb
+++ app/controllers/league_memberships_controller.rb
@@ -1,3 +1,17 @@
 class LeagueMembershipsController < ApplicationController
 
+  def create
+    @league = League.find(params[:league_id])
+
+    unless current_user
+      redirect_to new_user_path(redirect_to: league_path(@league)), notice: "Please sign up to join"
+      return
+    end
+
+    unless @league.members.include? current_user
+      @league.members << current_user
+    end
+
+    redirect_to league_path(@league)
+  end
 end
