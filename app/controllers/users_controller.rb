4043d7e8cbe0b609f2e46b7b881565cfe9a0fd93
Brett Huneycutt <brett@1000memories.com>
2013-09-03 21:01:04 -0700

    auto join league after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+
+      # join user to a league if specificed
+      if params[:league_id].presence
+        league = League.find(params[:league_id])
+        league.add_member(@user)  if league.hmac == params[:h]
+      end
+
       redirect_to params[:redirect_to].presence || new_league_path, notice: "Thank you for signing up!"
     else
       render "new"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  # TODO test this method
+  def link_to_join_league(league)
+    options = {
+      :class => "btn btn-main btn-xlg",
+    }
+
+    if current_user
+      path = league_memberships_path(@league)
+      options[:method] = "post"
+    else
+      path = new_user_path(redirect_to: league_path(league), league_id: league.id, h: league.hmac)
+    end
+
+    link_to "Join league", path, options
+  end
 end
