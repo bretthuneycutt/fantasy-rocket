1d5d41a763bda124ceca75bffe9c935d2b8bc6f1
Brett Huneycutt <brett@1000memories.com>
2013-09-07 13:31:39 -0700

    remove remember_me checkbox (remember by default)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,7 @@ class SessionsController < ApplicationController
 
     user = User.find_by_email(email)
     if user && user.authenticate(params[:password])
-      if params[:remember_me]
-        cookies.permanent[:auth_token] = user.auth_token
-      else
-        cookies[:auth_token] = user.auth_token
-      end
-
+      cookies.permanent[:auth_token] = user.auth_token
       redirect_to params[:redirect_to].presence || root_url
     else
       flash.now.alert = "Email or password is invalid"
