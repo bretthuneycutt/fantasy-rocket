026d637c977348958e6351b53dd827254cd6c145
Brett Huneycutt <brett@1000memories.com>
2013-09-16 14:31:29 -0700

    improve league_url and league_path using url_for

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,11 +1,17 @@
 module UrlHelper
-  def league_url(league, params = {})
-    root_url[0..-2] + league_path(league, params)
+  def league_url(league, options = {})
+    options[:controller] = 'leagues'
+    options[:action] = 'show'
+    options[:id] = league.id
+    options[:h] = league.hmac
+    options[:only_path] ||= false
+
+    url_for(options)
   end
 
-  def league_path(league, params = {})
-    params[:h] = league.hmac
-    "/leagues/#{league.to_param}?#{params.to_param}"
+  def league_path(league, options = {})
+    options[:only_path] = true
+    league_url(league, options)
   end
 
 end
