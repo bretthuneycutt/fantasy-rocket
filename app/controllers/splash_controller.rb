61f644f249225da01e1db105c24b2eae52f8f193
Brett Huneycutt <brett@1000memories.com>
2013-09-20 21:40:14 -0700

    only redirect to leagues of the same sport as subdomain

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,7 +1,7 @@
 class SplashController < ApplicationController
   def index
-    if current_user && current_user.leagues.size > 0
-      return redirect_to(current_user.leagues.first)
+    if current_user && league = current_user.leagues.where(sport: Sport.key).first
+      return redirect_to league
     end
   end
 end
