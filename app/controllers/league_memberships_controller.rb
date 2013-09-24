622a37f95e14952dd4ea75f542b31a7e82d61f11
Brett Huneycutt <brett@1000memories.com>
2013-09-23 23:32:51 -0700

    simplify some code and add a TODO

--- app/controllers/league_memberships_controller.rb
+++ app/controllers/league_memberships_controller.rb
@@ -1,8 +1,9 @@
 class LeagueMembershipsController < ApplicationController
+  before_filter :current_user!
 
+  # TODO require hmac to join league
   def create
     @league = League.find(params[:league_id])
-    raise Unauthorized  unless current_user
     @league.add_member(current_user)
     redirect_to league_path(@league)
   end
