53e1c840089d977b87272c1da927b3f08a2a2d9f
Brett Huneycutt <brett@1000memories.com>
2014-08-30 13:42:03 -0400

    rake db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130922183538) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "draft_picks", force: true do |t|
     t.integer  "league_id",  null: false
     t.integer  "member_id",  null: false
