f24316f3645e69b7d6806398f1d087c972fe46a7
Brett Huneycutt <brett@1000memories.com>
2014-08-30 16:47:54 -0400

    fix migrations

--- db/migrate/20140830202138_add_season_to_leagues.rb
+++ db/migrate/20140830202138_add_season_to_leagues.rb
@@ -1,6 +1,6 @@
 class AddSeasonToLeagues < ActiveRecord::Migration
   def change
-    add_column :leagues, :season, :string, default: false
+    add_column :leagues, :season, :string, null: false
     add_index :leagues, :season
     League.find_each do |l|
       l.update_attributes!(season: '2013')
